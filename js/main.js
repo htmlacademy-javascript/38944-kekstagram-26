64424ce7290e408e577d0388e740d2df5f4609bf
Natalia Ilina <ilinans@msk.bcs.ru>
2022-06-08 13:32:41 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const getRandomNumber = (min, max) => {
   if(typeof min !=='number' || typeof max !== 'number' || min <= 0 || max <= 0) {
-    throw new Error('Введены некорректные данные!');
+    return null;
   }
 
   //если значения равны
   if (min===max){
     return min;
   }
-  //если min > max - меняем их местами.
-  const minNumber = min < max ? min : max;
-  const maxNumber = min < max ? max : min;
+  //получаем минимальное и максимальное значение
+  const minNumber = Math.min(min, max);
+  const maxNumber = Math.max(min, max);
   // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 };
@@ -17,6 +17,6 @@ const getRandomNumber = (min, max) => {
 getRandomNumber(0, 1);
 
 
-const isRightLength = (str, maxLength) => typeof maxLength === 'number' && typeof str === 'string' && str.trim().length <= maxLength;
+const checkStringLength = (str, maxLength) => typeof maxLength === 'number' && typeof str === 'string' && str.trim().length <= maxLength;
 
-isRightLength('   qwertqwert  ', 10);
+checkStringLength('   qwertqwert  ', 10);
