c3684a64d31947fc18a288ba3998f4e15a077e07
Natalia Ilina <ilinans@msk.bcs.ru>
2022-06-11 10:29:52 +0300

    Модуляция

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+const checkStringLength = (str, maxLength) => typeof maxLength === 'number' && typeof str === 'string' && str.trim().length <= maxLength;
+
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+export {getRandomPositiveInteger};
