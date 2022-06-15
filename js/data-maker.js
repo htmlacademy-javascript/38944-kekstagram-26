a5117c3345c8bf21a7e81fa582181e84050f1ac5
Natalia Ilina <ilinans@msk.bcs.ru>
2022-06-15 17:24:25 +0300

    Отрисуй меня полностью (часть 1)

---
+++ js/data-maker.js
@@ -0,0 +1,28 @@
+import {createPhotoList} from './data.js';
+// Создаем массив временных данных
+const photos = createPhotoList();
+// Выбираем элемент, вутрь которого будем прибавлять созданные фото
+const picturesElement = document.querySelector('.pictures');
+// Выбираем элемень из template, который будем копировать
+const templateElement = document.querySelector('#picture').content.querySelector('.picture');
+
+const fragment = document.createDocumentFragment();
+
+// Создаем элементы пройдя по массиву временных данных
+photos.forEach((photo) => {
+  // Скопировали элемент
+  const photoElement = templateElement.cloneNode(true);
+
+  const imgElement = photoElement.querySelector('img');
+  const likesElement = photoElement.querySelector('.picture__likes');
+  const commentsElement = photoElement.querySelector('.picture__comments');
+
+  // Присваиваем значения
+  likesElement.textContent = photo.likes;
+  imgElement.src = photo.url;
+  commentsElement.textContent = photo.comments.length;
+
+  fragment.append(photoElement);
+});
+
+picturesElement.append(fragment);
