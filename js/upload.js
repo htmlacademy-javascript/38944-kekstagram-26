7da13c1d80d3fe60a207c4374ed61211489206db
Natalia Ilina <ilinans@msk.bcs.ru>
2022-07-01 15:09:03 +0300

    Надо подкачаться ТЗ п 3.3

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,8 @@
 import {isUploadFormValid} from './validator.js';
 import {isEscapeCode} from './utils.js';
 import {addScalingHandlers, removeScalingHandlers} from './scaling.js';
+import {setDefaultScaling} from './scaling.js';
+import {changeEffect, removeEffectListHandler} from './photo-effects.js';
 import './photo-effects.js';
 
 const uploadPopupElement = document.querySelector('.img-upload__overlay') ;
@@ -10,12 +12,20 @@ const formElement = document.querySelector('#upload-select-image');
 const hashtagsInputElement = document.querySelector('[name="hashtags"]');
 const commentTextareaElement = document.querySelector('[name="description"]');
 
+const resetForm = () => {
+  formElement.reset();
+  setDefaultScaling();
+  changeEffect('none');
+  removeEffectListHandler();
+};
+
 const onCancelButtonClick = () => {
   closePopup();
 };
 
 const onEscapeButtonDown = (evt) => {
   if (isEscapeCode(evt) && document.activeElement !== hashtagsInputElement && document.activeElement !== commentTextareaElement) {
+    resetForm();
     closePopup();
   }
 };
@@ -23,6 +33,8 @@ const onEscapeButtonDown = (evt) => {
 const onInputChange = () => {
   uploadPopupElement.classList.remove('hidden');
   document.body.classList.add('modal-open');
+
+  setDefaultScaling();
 
   document.addEventListener('keydown', onEscapeButtonDown);
   uploadCancelElement.addEventListener('click', onCancelButtonClick);
@@ -34,28 +46,37 @@ function closePopup () {
   uploadPopupElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
 
-  formElement.reset();
+  resetForm();
 
   uploadCancelElement.removeEventListener('click', onCancelButtonClick);
   document.removeEventListener('keydown', onEscapeButtonDown);
 
   removeScalingHandlers();
 }
-
 const onFormSubmit = (evt) => {
   evt.preventDefault();
   const isValid = isUploadFormValid();
   if (isValid) {
     // eslint-disable-next-line no-console
     console.log('Можно отправлять');
+    const formData = new FormData(evt.target);
+
+    // TODO зеленая + красная плашка, добавить catch, вынести в loader.js (sendData(data, onSuccess, onError))
+    fetch(
+      'https://26.javascript.pages.academy/kekstagram',
+      {
+        method: 'POST',
+        body: formData,
+      })
+      .then(closePopup);
+
   } else {
     // eslint-disable-next-line no-console
     console.log('Форма невалидна');
   }
 };
+// };
 
 uploadFileElement.addEventListener('change', onInputChange);
 
 formElement.addEventListener('submit', onFormSubmit);
-
-
